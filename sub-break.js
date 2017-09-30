630410b90d97218d1974f3938eebd4ac3943ed6c
SuburbanNinjaMC <suburbanninja575@gmail.com>
2017-09-30 15:08:32 -0400

    Update sub-break.js

--- sub-break.js
+++ sub-break.js
@@ -1,12 +1,14 @@
 javascript:
 (function() {
-  alert("break initialized");
+  alert("Break initialized. Press ` to get started.");
+  console.log("CONSOLE: Sub Break Initialized");
   function subBreak() {
     var y = prompt("Set the timer for the break (in minutes)");
       var a = y * 60 * 1000;
       if (y == 0) {
         var a = 10 * 60 * 1000;
       }
+      alert("Break Started. You have " + y + "minutes.");
       setTimeout(function(){ 
         for (var i = 0; i < 5; i++) {
 			     alert("Break is over. Get back to work :)"); 
